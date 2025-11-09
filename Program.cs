046baef6e1812688130b94fa9c9c3d0956c9e064
Chris0Jeky <jeky.tck@gmail.com>
2025-11-09 19:14:00 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,29 @@
-﻿using Raylib_cs;
+﻿Console.WriteLine("Hello, Lil Men!");
 
-Raylib.InitWindow(800, 450, "Raylib test");
-Raylib.SetTargetFPS(60);
+string name = "Chris";
+int count = 3;
+string? maybe = null;
+Console.WriteLine($"{name} x{count}");
+Console.WriteLine(maybe is null ? "is null" : maybe.Length.ToString());
 
-while (!Raylib.WindowShouldClose())
+// Console.WriteLine(); 
+// Console.Out.Flush();
+// string? input = Console.ReadLine();
+// Console.WriteLine($"You typed: {input}");
+
+static int Add(int a, int b) => a + b;
+static double Dist2(float x1, float y1, float x2, float y2) 
+    => (x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2);
+
+Console.WriteLine(Add(2, 5));
+Console.WriteLine(Dist2(0, 0, 3, 4));
+
+static string SpeedBand(float v) => v switch
 {
-    Raylib.BeginDrawing();
-    Raylib.ClearBackground(Color.Black);
-    Raylib.DrawCircle(400, 225, 50, Color.SkyBlue);
-    Raylib.DrawFPS(10, 10);
-    Raylib.EndDrawing();
-}
-Raylib.CloseWindow();
+    < 0.2f => "slow",
+    < 1.0f => "average",
+    < 2.0f => "fast",
+    _ => "ultra fast"
+}; 
+
+Console.WriteLine(SpeedBand(0.05f));
