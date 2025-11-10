616b084830f52723ebc59c384b2bb05b88b60b35
Chris0Jeky <jeky.tck@gmail.com>
2025-11-10 03:07:20 +0000

    Create Program.cs

---
+++ SwarmSim.Benchmarks/Program.cs
@@ -0,0 +1,13 @@
+using BenchmarkDotNet.Running;
+
+namespace SwarmSim.Benchmarks;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("SwarmSim.Benchmarks - BenchmarkDotNet suite");
+        Console.WriteLine("TODO: Add benchmark classes and uncomment line below");
+        // BenchmarkRunner.Run<YourBenchmarkClass>();
+    }
+}
