112d656b048cf99bc1f7f1e752ff749271d78823
Chris0Jeky <jeky.tck@gmail.com>
2025-11-10 03:07:22 +0000

    Create Program.cs

---
+++ SwarmSim.Render/Program.cs
@@ -0,0 +1,10 @@
+namespace SwarmSim.Render;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("SwarmSim.Render - Raylib visualization");
+        Console.WriteLine("TODO: Implement rendering loop");
+    }
+}
